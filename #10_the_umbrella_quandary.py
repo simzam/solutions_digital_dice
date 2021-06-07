414e89ec72dfbe65464c3474ccd34120411f52e3
Simon Iversen <simon.iversen@protonmail.com>
2021-06-07 09:21:54 +0200

    Solve problem. Fix bug.
    Notice the use of deepcopy. Need to think a little on how lists are
    stored in Python. Thought I could keep a copy inside a function just
    by reassignment. Need copy in order not to disturb the data.

--- #10_the_umbrella_quandary.py
+++ #10_the_umbrella_quandary.py
@@ -1,13 +1,8 @@
-
 import numpy as np
 import random
+import copy
 import matplotlib.pyplot as plt
 
-points = 80
-
-probabilities = np.linspace(0.01, 0.99, points)
-averages = np.zeros(points)
-simulations = 30000
 
 def move_umbrella(position, umbrellas):
     if position == 0: # home
@@ -22,19 +17,38 @@ def move_umbrella(position, umbrellas):
             return umbrellas
     return False
 
-for i, prob in enumerate(probabilities):
-    counter = 0
-    for _ in range(simulations):
-        umbrellas = [2, 2]
-        position = 0 # 0 reps home, 1 reps office
-        while umbrellas:
-            decide_weather = random.random()
-            if decide_weather < prob:
-                umbrellas = move_umbrella(position, umbrellas)
 
-            position = (position + 1) % 2
-            counter += 1
-    averages[i] = counter / simulations
-        
-plt.plot(probabilities, averages)
-plt.show()
+def umbrella_sim(init_umbrellas, probabilities, points):
+    averages = np.zeros(points)
+    simulations = 10000
+
+    for i, prob in enumerate(probabilities):
+        counter = 0
+        for _ in range(simulations):
+            umbrellas = copy.deepcopy(init_umbrellas)
+            position = 0 # 0 reps home, 1 reps office
+            while umbrellas != False:
+                decide_weather = random.random()
+
+                if decide_weather < prob:
+                    umbrellas = move_umbrella(position, umbrellas)
+                position = (position + 1) % 2
+                counter += 1
+        averages[i] = counter / simulations
+
+    return averages
+
+    
+if __name__ == '__main__':
+    points = 200
+    probabilities = np.linspace(0.01, 0.99, points)
+    avg1 = umbrella_sim([1,1], probabilities, points)
+
+    avg2 = umbrella_sim([2, 2], probabilities, points)
+
+    plt.plot(probabilities, avg1)
+    plt.plot(probabilities, avg2)
+    plt.ylim((-10,200))
+    plt.show()
+
+
