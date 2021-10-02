297a9739380cc086a965da8cbccdb4ef9f0b9e9f
Simon Iversen <simon.iversen@protonmail.com>
2021-10-02 19:14:37 +0200

    Add test for "add_lists".

--- p21_test.py
+++ p21_test.py
@@ -1,10 +1,21 @@
 import unittest
-from p21 import generate_sons
+from p21 import generate_sons_probs, add_lists
 
 
 class test_problem20(unittest.TestCase):
     def test_generate_sons(self):
-        generate_sons
+        pass
+
+    def test_add_lists(self):
+        lst1 = [1, 2, 3]
+        lst2 = [4, 5, 6]
+        self.assertEqual(add_lists(lst1, lst2), [5, 7, 9])
+
+        lst2 = [3, 4]
+        self.assertEqual(add_lists(lst1, lst2), [4, 6, 3])
+
+        lst1 = []
+        self.assertEqual(add_lists(lst1, lst2), [3, 4])
 
 
 if __name__ == '__main__':
