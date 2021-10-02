f3ea4883bc74ea04b12fbd3b7a230b0918bef06f
Simon Iversen <simon.iversen@protonmail.com>
2021-10-02 18:58:11 +0200

    * p21.py: Add test file for problem 21

---
+++ p21_test.py
@@ -0,0 +1,11 @@
+import unittest
+from p21 import generate_sons
+
+
+class test_problem20(unittest.TestCase):
+    def test_generate_sons(self):
+        generate_sons
+
+
+if __name__ == '__main__':
+    unittest.main()
