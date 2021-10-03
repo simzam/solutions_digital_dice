6bb12a8db584fee4b07a26549b61cdb160d65120
Simon Iversen <simon.iversen@protonmail.com>
2021-10-03 21:35:43 +0200

    Rewrite problem.
    Solve the problem of the number of generations below having a given
    number of children.

--- p21.py
+++ p21.py
@@ -2,23 +2,23 @@
 Problem 21: Chain Reactions, Branching Processes, and Baby Boys
 
 
+Does not actually solve problenm 21.
 """
 import random
 
 
-def experiment(probs, stop_gen=10):
-    male_descendants = []
-    for idx_generations in range(stop_gen):
-        if len(male_descendants) == 0:
-            sons = generate_sons(probs)
-        else:
-            sons = 0
-            for _ in range(male_descendants[idx_generations - 1]):
-                sons += generate_sons(probs)
-        if sons == 0:
-            break
-        male_descendants.append(sons)
-    return male_descendants
+def experiment(reg, probs):
+    prev_gen_sons = 1
+    for i, generation in enumerate(reg):
+        next_gen_sons = 0
+        for _ in range(prev_gen_sons):
+            tmp = generate_sons(probs)
+            next_gen_sons += tmp
+            generation[tmp] += 1
+            reg[i] = generation
+        if next_gen_sons == 0:
+            return
+        prev_gen_sons = next_gen_sons
 
 
 def generate_sons(sons_prob):
@@ -28,11 +28,11 @@ def generate_sons(sons_prob):
         if draw_sons < prob:
             break
         num_sons += 1
-    return num_sons
+    return num_sons % 3
 
 
 def generate_sons_probs(max_sons):
-    """ generates the probabiliies for a man to have 0 to "max_sons".
+    """Generates the probabiliies for a man to have 0 to "max_sons".
     Note "sum(son_probs) != 1". """
     sons_probs = [0.4825]
     for i in range(max_sons):
@@ -41,34 +41,25 @@ def generate_sons_probs(max_sons):
     return sons_probs
 
 
-def add_lists(lst1, lst2):
-    retlst = []
-    for elem1, elem2 in zip(lst1, lst2):
-        retlst.append(elem1 + elem2)
-    if len(lst1) > len(lst2):
-        return retlst + lst1[len(lst2):]
-    else:
-        return retlst + lst2[len(lst1):]
+def setup_register(generations, max_num_sons=7):
+    """Create a list of dicts. """
+    reg = [{i: 0 for i in range(max_num_sons)} for _ in range(generations)]
+
+    return reg
 
 
 def main():
-    simulations = 1000
+    simulations = 100000
+
+    max_generations = 10
     max_sons = 7
+
     probs = generate_sons_probs(max_sons)
-    total_sons = []
+    register = setup_register(max_generations, max_sons)
+
     for i in range(simulations):
-        # print(len(total_sons))
-        male_offspring = experiment(probs)
-        if len(male_offspring) == 0:
-            continue
-        #print(i, male_offspring, total_sons)
-        tmp_sons = [elem_prev + elem for elem_prev,
-                    elem in zip(total_sons, male_offspring)]
-        if len(male_offspring) > len(total_sons):
-            total_sons = tmp_sons + male_offspring[len(total_sons):]
-        else:
-            total_sons = tmp_sons + total_sons[len(male_offspring):]
-    return list(map(lambda x: x/simulations, total_sons))
+        experiment(register, probs)
+    return register
 
 
 if __name__ == '__main__':
