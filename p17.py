22021c0addd3cc0131a2db497fc5d1a62ed357f7
Simon Iversen <simon.iversen@protonmail.com>
2021-10-04 13:36:53 +0200

    Speed up program
    Restructure functions and avoid endless amounts of costly list operations

--- p17.py
+++ p17.py
@@ -1,4 +1,11 @@
+"""
+Problem 17: Waiting for the Bus
+
+Given that a bus leaves from the bus stop at every hour.
+"""
+
 import random
+
 
 def calc_waiting_time(traveller, buses):
     for bus in buses:
@@ -8,12 +15,27 @@ def calc_waiting_time(traveller, buses):
     return 60 - traveller
 
 
+def generate_random_hourly():
+    return int((random.random() * 60) // 1)
 
-vals = [i for i in range(60)]
-for i in range(10):
-    waiting_time = 0
-    for _ in range(1000000):
-        buses = sorted([0] + [random.choice(vals) for _ in range(i)])
-        traveller = random.choice(vals)
-        waiting_time += calc_waiting_time(traveller, buses)
-    print(i+1, waiting_time / 1000000 / 60)
+
+def generate_buses(num_buses):
+    buses = [0]
+    for _ in range(num_buses):
+        buses.append(generate_random_hourly())
+    return sorted(buses)
+
+
+def main():
+    simulations = 100000
+    for i in range(10):
+        waiting_time = 0
+        for _ in range(simulations):
+            buses = generate_buses(i)
+            traveller = generate_random_hourly()
+            waiting_time += calc_waiting_time(traveller, buses)
+        print(i+1, waiting_time / simulations / 60)
+
+
+if __name__ == '__main__':
+    main()
