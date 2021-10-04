3df7da416d7000876d935c8f086fae11d22ae120
Simon Iversen <simon.iversen@protonmail.com>
2021-10-04 13:43:20 +0200

    Write problem description.

--- p17.py
+++ p17.py
@@ -1,7 +1,10 @@
-"""
-Problem 17: Waiting for the Bus
+"""Problem 17: Waiting for the Bus
 
-Given that a bus leaves from the bus stop at every hour.
+Given that a bus leaves from the bus stop at every hour. Given that
+there are a number of additional buses arriving every hour + random
+number of minutes between 0 and 59, How long is the waiting time in
+hours.
+
 """
 
 import random
@@ -28,13 +31,15 @@ def generate_buses(num_buses):
 
 def main():
     simulations = 100000
+    print("i: number of buses\nwait: waiting time in hours\n")
     for i in range(10):
         waiting_time = 0
         for _ in range(simulations):
             buses = generate_buses(i)
             traveller = generate_random_hourly()
             waiting_time += calc_waiting_time(traveller, buses)
-        print(i+1, waiting_time / simulations / 60)
+        print("i={:2}, wait={:1.4}".format(
+            i + 1, waiting_time / simulations / 60))
 
 
 if __name__ == '__main__':
